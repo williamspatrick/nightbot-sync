20a8e06cbe8841caaa19b10c7f52ba123f11ccb7
Patrick Williams <patrick@stwcx.xyz>
2022-04-04 08:45:04 -0500

    dotenv: import nightbot oauth2 params
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import 'dotenv/config';
+
+if (
+    !('NIGHTBOT_CLIENT_ID' in process.env)
+    || !('NIGHTBOT_CLIENT_SECRET' in process.env)
+) {
+    console.log("Couldn't find Nightbot settings");
+    console.log('   Add NIGHTBOT_CLIENT_ID and NIGHTBOT_CLIENT_SECRET');
+    process.exit(1);
+}
